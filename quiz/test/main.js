61a4544da7efc2884a122c94cd17d02f8ca4c153
drafti <39197572+Dimasi4ek@users.noreply.github.com>
2022-02-22 22:07:48 +0200

    new

--- quiz/test/main.js
+++ quiz/test/main.js
@@ -1,4 +1,4 @@
-																$(document).ready(function() {
+$(document).ready(function() {
 
 	$('.step-slide__title').each(function(index, el) {
 		$(el).prepend('<div class="step-slide__title-counter">' + (index + 1) + '</div>');
