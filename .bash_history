99c502e15a34a122c6204509040624442857546e
drafti <h0661560102@gmail.com>
2023-10-10 16:49:22 +0300

    м

---
+++ .bash_history
@@ -0,0 +1,6 @@
+#1650203881
+find public_html/ -type d -exec chmod 755 {} \;
+#1650203889
+find public_html/ -type f -exec chmod 644 {} \;
+#1650279890
+mc
--- send.php
+++ send.php
@@ -1,25 +1,26 @@
 <?php
 
-/* https://api.telegram.org/bot5347896717:AAFZzqTOh8Ik3mQLWSpsZ5FBC2edybqfmtc/getUpdates,
+/* https://api.telegram.org/bot6070015133:AAE2MwIVcMF8bv1VevtYQMRu0pCefr4NyDw/getUpdates,
 где, 5347896717:AAFZzqTOh8Ik3mQLWSpsZ5FBC2edybqfmtc - токен вашего бота, полученный ранее */
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
+$name = $_POST['name'];
+$phone = $_POST['tell'];
+$vid = $_POST['vid'];
 
-
-$token = "";
-$chat_id = "-";
+$token = "6070015133:AAE2MwIVcMF8bv1VevtYQMRu0pCefr4NyDw";
+$chat_id = "-605017734";
 
 $arr = array(
   'Имя: ' => $name,
   'Телефон: ' => "$phone",
+  'Форма отправленна с: ' => "$vid",
 );
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
+foreach ($arr as $key => $value) {
+  $txt .= "<b>" . $key . "</b> " . $value . "%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
 
 // if ($sendToTelegram) {
 //   echo "Успешно отправленно";;
